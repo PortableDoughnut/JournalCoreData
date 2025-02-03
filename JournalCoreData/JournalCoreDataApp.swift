3ef6d5c8c1e5da3d2fed1bff7300dc7eedee82b0
PortableDoughnut <gwen.thelin@gmail.com>
2025-02-03 07:53:23 -0700

    Initial Commit

---
+++ JournalCoreData/JournalCoreDataApp.swift
@@ -0,0 +1,20 @@
+//
+//  JournalCoreDataApp.swift
+//  JournalCoreData
+//
+//  Created by Gwen Thelin on 2/3/25.
+//
+
+import SwiftUI
+
+@main
+struct JournalCoreDataApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
